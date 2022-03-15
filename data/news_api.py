aae2f3c909571f57cca41aab0b05a5bb5a396181
jur4ikoff <123456>
2022-03-15 18:34:55 +0300

    api добавление новостей и тесты

--- data/news_api.py
+++ data/news_api.py
@@ -1,6 +1,7 @@
 import flask
-from flask import jsonify
+from flask import jsonify, request
 import sqlalchemy_serializer
+import datetime
 from . import db_session
 from .news import News
 from .jobs import Jobs
@@ -24,6 +25,38 @@ def get_news():
                  for item in news]
         }
     )
+
+
+@blueprint.route('/api/news', methods=['POST'])
+def create_news():
+    db_session.global_init("db/register.db")
+    if not request.json:
+        return jsonify({'error': 'Empty request'})
+    elif not all(key in request.json for key in
+                 ['title', 'content', 'user_id', 'is_private']):
+        return jsonify({'error': 'Bad request'})
+    db_sess = db_session.create_session()
+    news = News(
+        title=request.json['title'],
+        content=request.json['content'],
+        user_id=request.json['user_id'],
+        is_private=request.json['is_private']
+    )
+    db_sess.add(news)
+    db_sess.commit()
+    return jsonify({'success': 'OK'})
+
+
+@blueprint.route('/api/news/<int:news_id>', methods=['DELETE'])
+def delete_news(news_id):
+    db_session.global_init("db/register.db")
+    db_sess = db_session.create_session()
+    news = db_sess.query(News).get(news_id)
+    if not news:
+        return jsonify({'error': 'Not found'})
+    db_sess.delete(news)
+    db_sess.commit()
+    return jsonify({'success': 'OK'})
 
 
 @blueprint.route('/api/news/<int:news_id>', methods=['GET'])
@@ -51,3 +84,45 @@ def get_jobs():
                  for item_jobs in jobs]
         }
     )
+
+
+@blueprint.route('/api/jobs/<int:jobs_id>', methods=['GET'])
+def get_one_jobs(jobs_id):
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(Jobs).get(jobs_id)
+    if not jobs:
+        return jsonify({'error': 'Not found'})
+    return jsonify(
+        {
+            'jobs': jobs.to_dict(only=('team_leader', 'job', 'work_size', 'collaborators', 'is_finished'))
+        }
+    )
+
+
+@blueprint.route('/api/jobs', methods=['POST'])
+def create_jobs():
+    db_session.global_init("db/register.db")
+    db_sess = db_session.create_session()
+    jobs_id = db_sess.query(Jobs).all()
+    if not request.json:
+        return jsonify({'error': 'Empty request'})
+    elif not all(key in request.json for key in
+                 ['id', 'team_leader', 'job', 'work_size', 'collaborators', 'is_finished']):
+        return jsonify({'error': 'Bad request'})
+    elif request.json["is_finished"] not in (0, 1):
+        return jsonify({'error': 'is_finished not Boolean'})
+    elif request.json:
+        for user in db_sess.query(Jobs).filter(Jobs.id == request.json["id"]):
+            if user:
+                return jsonify({'error': 'Id already exists'})
+    jobs = Jobs(
+        id=request.json['id'],
+        team_leader=request.json['team_leader'],
+        job=request.json['job'],
+        work_size=request.json['work_size'],
+        collaborators=request.json['collaborators'],
+        is_finished=request.json['is_finished']
+    )
+    db_sess.add(jobs)
+    db_sess.commit()
+    return jsonify({'success': 'OK'})
